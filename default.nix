d84af29ad6633895f0b0bd3ce44135f9e1e5b05e
bohendo <bohendo@pm.me>
2022-11-16 16:29:54 -0500

    convert to typescript & build with nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+{
+  pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz";
+    sha256 = "sha256:0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
+  }) {}
+}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "phonetic";
+  version = "0.0.1";
+
+  src = ./.;
+
+  buildInputs = [
+    pkgs.nodejs
+    pkgs.nodePackages.typescript
+  ];
+
+  buildPhase = ''
+    tsc index.ts
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mv index.js $out/bin/phonetic
+    chmod +x $out/bin/phonetic
+  '';
+
+}
---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const prefixes = 'dozmarbinwansamlitsighidfidlissogdirwacsabwissibrigsoldopmodfoglidhopdardorlorhodfolrintogsilmirholpaslacrovlivdalsatlibtabhanticpidtorbolfosdotlosdilforpilramtirwintadbicdifrocwidbisdasmidloprilnardapmolsanlocnovsitnidtipsicropwitnatpanminritpodmottamtolsavposnapnopsomfinfonbanmorworsipronnorbotwicsocwatdolmagpicdavbidbaltimtasmalligsivtagpadsaldivdactansidfabtarmonranniswolmispallasdismaprabtobrollatlonnodnavfignomnibpagsopralbilhaddocridmocpacravripfaltodtiltinhapmicfanpattaclabmogsimsonpinlomrictapfirhasbosbatpochactidhavsaplindibhosdabbitbarracparloddosbortochilmactomdigfilfasmithobharmighinradmashalraglagfadtopmophabnilnosmilfopfamdatnoldinhatnacrisfotribhocnimlarfitwalrapsarnalmoslandondanladdovrivbacpollaptalpitnambonrostonfodponsovnocsorlavmatmipfip'
+// console.log(`prefixes length: ${prefixes.length} aka ${prefixes.length / 3}`)
+
+const suffixes = 'zodnecbudwessevpersutletfulpensytdurwepserwylsunrypsyxdyrnuphebpeglupdepdysputlughecryttyvsydnexlunmeplutseppesdelsulpedtemledtulmetwenbynhexfebpyldulhetmevruttylwydtepbesdexsefwycburderneppurrysrebdennutsubpetrulsynregtydsupsemwynrecmegnetsecmulnymtevwebsummutnyxrextebfushepbenmuswyxsymselrucdecwexsyrwetdylmynmesdetbetbeltuxtugmyrpelsyptermebsetdutdegtexsurfeltudnuxruxrenwytnubmedlytdusnebrumtynseglyxpunresredfunrevrefmectedrusbexlebduxrynnumpyxrygryxfeptyrtustyclegnemfermertenlusnussyltecmexpubrymtucfyllepdebbermughuttunbylsudpemdevlurdefbusbeprunmelpexdytbyttyplevmylwedducfurfexnulluclennerlexrupnedlecrydlydfenwelnydhusrelrudneshesfetdesretdunlernyrsebhulrylludremlysfynwerrycsugnysnyllyndyndemluxfedsedbecmunlyrtesmudnytbyrsenwegfyrmurtelreptegpecnelnevfes'
+// console.log(`suffixes length: ${suffixes.length} aka ${suffixes.length / 3}`)
+
+const getRandomByte = (): number => Math.floor(Math.random() * 256);
+
+const grn = (): string => {
+  const i = getRandomByte() * 3;
+  const j = getRandomByte() * 3;
+  const name = `${prefixes.substring(i, i+3)}${suffixes.substring(j, j+3)}`;
+  console.log(`${i} + ${j} = ${name}`)
+  return name;
+};
+
+console.log(`Random name: ${grn()}-${grn()}-${grn()}-${grn()}`)
